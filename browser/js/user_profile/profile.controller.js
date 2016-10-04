851353d2e669605e9972272e670e3f0bc342944d
Nathan Wallace <ncw232@gmail.com>
2016-10-04 17:46:03 -0400

    user game record

--- browser/js/user_profile/profile.controller.js
+++ browser/js/user_profile/profile.controller.js
@@ -4,11 +4,29 @@ app.config(function($stateProvider){
 		templateUrl:"js/user_profile/profile.template.html",
 		controller: "UserCtrl"
 	})
+	$stateProvider.state("GameRecord", {
+		url:"/users/:userId/games",
+		templateUrl: "js/user_profile/games.html",
+		controller: "GameRecordCtrl"
+	})
 })
 
 app.controller("UserCtrl", function($scope, UserFactory, $stateParams){
 	UserFactory.fetchInformation($stateParams.userId)
 	.then(function(user){
 		$scope.user=user;
+	})
+})
+
+app.controller("GameRecordCtrl",function($scope, UserFactory, $stateParams){
+	UserFactory.fetchInformation($stateParams.userId)
+	.then(function(user){
+		$scope.user=user;
+	})
+	.then(function(user){
+	UserFactory.fetchGames($stateParams.userId)
+	})
+	.then(function(games){
+		$scope.games=games;
 	})
 })
