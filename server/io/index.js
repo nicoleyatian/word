abd8b529594511897018f53c42fcea337be1a037
Darell Koh <darellkoh@gmail.com>
2016-10-05 15:31:40 -0400

    added authentication functionality to game controller and fixed joinRoom button

--- server/io/index.js
+++ server/io/index.js
@@ -27,7 +27,7 @@ module.exports = function(server) {
 
             socket.join(roomName);
             console.log('A client joined this room: ', roomName);
-            io.to(roomName).emit('roomJoinSuccess', user);
+            socket.broadcast.to(roomName).emit('roomJoinSuccess', user);
             // io.sockets.in(roomName).emit('roomData', {
             //     count: io.sockets.adapter.rooms[roomName]
             // })
