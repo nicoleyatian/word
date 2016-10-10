02e374c26414b51b6b2a63903d43abe190545cf0
Darell Koh <darellkoh@gmail.com>
2016-10-10 15:26:01 -0400

    created letter directive - in progress

--- browser/js/game-state/grid.controller.js
+++ browser/js/game-state/grid.controller.js
@@ -33,32 +33,42 @@ app.controller('GameCtrl', function($scope, BoardFactory, Socket, $stateParams, 
         pointsEarned: null
     };
 
-    $scope.mouseIsDown = false;
-    $scope.draggingAllowed = false;
-    $scope.style=null;
-    $scope.message='';
-    $scope.freeze=false;
-    $scope.winOrLose=null;
-    $scope.timeout=null;
-    $scope.isSelecting = false;
+    $scope.style = null;
+    $scope.message = '';
+    $scope.freeze = false;
+    $scope.winOrLose = null;
+    $scope.timeout = null;
 
 
-    $scope.checkSelected = function(id) {
-        return id in $scope.exports.wordObj;
-    };
+    // $scope.checkSelected = function(id) {
+    //     return id in $scope.exports.wordObj;
+    // };
 
     $scope.toggleDrag = function() {
         $scope.draggingAllowed = !$scope.draggingAllowed;
     };
 
-    $scope.mouseDown = function() {
-        $scope.mouseIsDown = true;
-    };
+    // $scope.mouseDown = function() {
+    //     console.log('mouse is down')
+    //     $scope.mouseIsDown = true;
+    // };
 
-    $scope.mouseUp = function() {
-        $scope.mouseIsDown = false;
-        if ($scope.draggingAllowed && $scope.exports.word.length > 1) $scope.submit($scope.exports);
-    };
+    // $scope.mouseUp = function() {
+    //     console.log('mouse is up');
+    //     $scope.mouseIsDown = false;
+    //     if ($scope.draggingAllowed && $scope.exports.word.length > 1) $scope.submit($scope.exports);
+    // };
+
+    // $scope.touchActivated = function() {
+    //     console.log('touch is activated: ' + arguments);
+    //     $scope.touchIsActivated = true;
+    // }
+
+    // $scope.touchStopped = function(e) {
+    //     console.log('touch is stopped: ' + e);
+    //     $scope.touchIsActivated = false;
+    //     if ($scope.draggingAllowed && $scope.exports.word.length > 1) $scope.submit($scope.exports);
+    // }
 
     // $element.bind('touchstart', function (e) {
     //   $scope.isSelecting = true;
@@ -69,7 +79,7 @@ app.controller('GameCtrl', function($scope, BoardFactory, Socket, $stateParams, 
     //   if ($scope.isSelecting) {
     //     $scope.click(e)
     //   }
-    // })
+    // })x
 
     // $element.bind('mouseup touchend', function (e) {
     //   $scope.isSelecting = false;
@@ -77,31 +87,117 @@ app.controller('GameCtrl', function($scope, BoardFactory, Socket, $stateParams, 
     // })
 
 
-    $scope.drag = function(space, id) {
-        if ($scope.mouseIsDown && $scope.draggingAllowed) {
-            $scope.click(space, id);
-        }
-    };
+    // $scope.drag = function(space, id) {
+    //     console.log('mouse enter: ' + id);
+    //     if ($scope.mouseIsDown && $scope.draggingAllowed) {
+    //         $scope.click(space, id);
+    //     }
+    // };
 
-    $scope.click = function(space, id) {
-        if ($scope.freeze) {
-            return; }
-        console.log('clicked ', space, id);
-        var ltrsSelected = Object.keys($scope.exports.wordObj);
-        var previousLtr = ltrsSelected[ltrsSelected.length - 2];
-        var lastLtr = ltrsSelected[ltrsSelected.length - 1];
-        if (!ltrsSelected.length || validSelect(id, ltrsSelected)) {
-            $scope.exports.word += space;
-            $scope.exports.wordObj[id] = space;
-            console.log($scope.exports);
-        } else if (id === previousLtr) {
-            $scope.exports.word = $scope.exports.word.substring(0, $scope.exports.word.length - 1);
-            delete $scope.exports.wordObj[lastLtr];
-        } else if (ltrsSelected.length === 1 && id === lastLtr) {
-            $scope.exports.word = "";
-            delete $scope.exports.wordObj[lastLtr];
-        }
-    };
+    // function div_overlap(jqo, left, top) {
+    //     console.log('div overlapped: ' + jqo);
+    //     var d = jqo.offset();
+    //     return top >= d.top && left >= d.left && left <= (d.left+jqo[0].offsetWidth) && top <= (d.top+jqo[0].offsetHeight);
+    // }
+
+    // touchmove = function(event) {
+    //     // Prevent scrolling on this element
+    //     event.preventDefault();
+    // }
+
+    // $(".cell").bind("mouseenter touchmove", function(evt){
+    //     console.log('binding mouseenter and touchmove', evt);
+    //     $(".cell").each(function() {
+    //         console.log('for each element');
+    //        if (div_overlap(this, evt.pageX, evt.pageY)){
+    //         console.log('entering div_overlap');
+    //           if (!this.hasClass('selected')) {
+    //             this.addClass('selected');
+    //           }
+    //        }
+    //     });
+    // });
+
+    // angular.element('.cell').on("click", function(evt){
+    //     console.log('binding mouseenter and touchmove', evt);
+        // $(".cell").each(function() {
+        //     console.log('for each element');
+        //    if (div_overlap(this, evt.pageX, evt.pageY)){
+        //     console.log('entering div_overlap');
+        //       if (!this.hasClass('selected')) {
+        //         this.addClass('selected');
+        //       }
+        //    }
+        // });
+    // });
+
+    // $element.children()(function(evt){
+    //     console.log('binding mouseenter and touchmove', evt);
+        // $(".cell").each(function() {
+        //     console.log('for each element');
+        //    if (div_overlap(this, evt.pageX, evt.pageY)){
+        //     console.log('entering div_overlap');
+        //       if (!this.hasClass('selected')) {
+        //         this.addClass('selected');
+        //       }
+        //    }
+        // });
+    // });
+
+
+    // $element.bind("touchmove", function(evt){
+    //     console.log('binding mouseenter and touchmove', evt);
+    //     // $(".cell").each(function() {
+    //     //     console.log('for each element');
+    //     //    if (div_overlap(this, evt.pageX, evt.pageY)){
+    //     //     console.log('entering div_overlap');
+    //     //       if (!this.hasClass('selected')) {
+    //     //         this.addClass('selected');
+    //     //       }
+    //     //    }
+    //     // });
+    // });
+
+    // angular.element('.cell').bind("touchmove", function(evt){
+    //     console.log('binding mouseenter and touchmove', evt);
+    //     angular.element('.cell').each(function() {
+    //         console.log('for each element');
+    //        if (div_overlap(this, evt.pageX, evt.pageY)){
+    //         console.log('entering div_overlap');
+    //           if (!this.hasClass('selected')) {
+    //             this.addClass('selected');
+    //           }
+    //        }
+    //     });
+    // });
+
+    // $scope.mobileDrag = function(space, id){
+    //     console.log('touch is dragged: ' + space + " : " + id);
+    //     if($scope.touchIsActivated && $scope.draggingAllowed){
+    //         $scope.click(space, id);
+    //     }
+    // };
+
+    // $scope.click = function(space, id) {
+    //     if ($scope.freeze) {
+    //         return;
+    //     }
+    //     console.log('clicked ', space, id);
+    //     var ltrsSelected = Object.keys($scope.exports.wordObj);
+    //     var previousLtr = ltrsSelected[ltrsSelected.length - 2];
+    //     var lastLtr = ltrsSelected[ltrsSelected.length - 1];
+    //     if (!ltrsSelected.length || validSelect(id, ltrsSelected)) {
+    //         $scope.exports.word += space;
+    //         $scope.exports.wordObj[id] = space;
+    //         console.log($scope.exports);
+    //     } else if (id === previousLtr) {
+    //         $scope.exports.word = $scope.exports.word.substring(0, $scope.exports.word.length - 1);
+    //         delete $scope.exports.wordObj[lastLtr];
+    //     } else if (ltrsSelected.length === 1 && id === lastLtr) {
+    //         $scope.exports.word = "";
+    //         delete $scope.exports.wordObj[lastLtr];
+    //     }
+    // };
 
 
     //get the current room info
@@ -149,19 +245,19 @@ app.controller('GameCtrl', function($scope, BoardFactory, Socket, $stateParams, 
 
 
     //makes sure letter is adjacent to prev ltr, and hasn't been used yet
-    function validSelect(ltrId, otherLtrsIds) {
-        if (otherLtrsIds.includes(ltrId)) return false;
-        var coords = ltrId.split('-');
-        var row = coords[0];
-        var col = coords[1];
-        var lastLtrId = otherLtrsIds.pop();
-        var coordsLast = lastLtrId.split('-');
-        var rowLast = coordsLast[0];
-        var colLast = coordsLast[1];
-        var rowOffset = Math.abs(row - rowLast);
-        var colOffset = Math.abs(col - colLast);
-        return (rowOffset <= 1 && colOffset <= 1);
-    }
+    // function validSelect(ltrId, otherLtrsIds) {
+    //     if (otherLtrsIds.includes(ltrId)) return false;
+    //     var coords = ltrId.split('-');
+    //     var row = coords[0];
+    //     var col = coords[1];
+    //     var lastLtrId = otherLtrsIds.pop();
+    //     var coordsLast = lastLtrId.split('-');
+    //     var rowLast = coordsLast[0];
+    //     var colLast = coordsLast[1];
+    //     var rowOffset = Math.abs(row - rowLast);
+    //     var colOffset = Math.abs(col - colLast);
+    //     return (rowOffset <= 1 && colOffset <= 1);
+    // }
 
     function clearIfConflicting(updateWordObj, exportWordObj) {
         var tilesMoved = Object.keys(updateWordObj);
@@ -214,23 +310,22 @@ app.controller('GameCtrl', function($scope, BoardFactory, Socket, $stateParams, 
     $scope.update = function(updateObj) {
         $scope.updateScore(updateObj.pointsEarned, updateObj.playerId);
         $scope.updateBoard(updateObj.wordObj);
-        if (+$scope.user.id===+updateObj.playerId){
-            var player=$scope.user.username;
-        }
-        else{
-            for (var key in $scope.otherPlayers){
-                if (+$scope.otherPlayers[key].id===+updateObj.playerId){
-                    var player=$scope.otherPlayers[key].username;
+        if (+$scope.user.id === +updateObj.playerId) {
+            var player = $scope.user.username;
+        } else {
+            for (var key in $scope.otherPlayers) {
+                if (+$scope.otherPlayers[key].id === +updateObj.playerId) {
+                    var player = $scope.otherPlayers[key].username;
                     break;
                 }
             }
         }
-        $scope.message=player+" played "+updateObj.word+" for "+updateObj.pointsEarned+" points!";
-        if ($scope.timeout){
+        $scope.message = player + " played " + updateObj.word + " for " + updateObj.pointsEarned + " points!";
+        if ($scope.timeout) {
             clearTimeout($scope.timeout);
         }
-        $scope.timeout=setTimeout(function(){
-            $scope.message='';
+        $scope.timeout = setTimeout(function() {
+            $scope.message = '';
         }, 3000)
         console.log('its updating!');
         clearIfConflicting(updateObj, $scope.exports.wordObj);
@@ -238,46 +333,42 @@ app.controller('GameCtrl', function($scope, BoardFactory, Socket, $stateParams, 
         $scope.$evalAsync();
     };
 
-    $scope.replay=function(){
+    $scope.replay = function() {
         LobbyFactory.newGame($scope.roomName);
         $scope.startGame();
     }
 
-    $scope.determineWinner=function(winnersArray){
-        if (winnersArray.length===1){
-            if (+winnersArray[0]===+$scope.user.id){
-                $scope.winOrLose="Congratulation! You are a word wizard! You won!!!";
-            }
-            else{
-                for (var player in $scope.otherPlayers){
-                    if (+$scope.otherPlayers[player].id===+winnersArray[0]){
-                        var winner=$scope.otherPlayers[player].username;
-                        $scope.winOrLose="Tough luck. "+winner+" has beaten you. Better Luck next time. :("
+    $scope.determineWinner = function(winnersArray) {
+        if (winnersArray.length === 1) {
+            if (+winnersArray[0] === +$scope.user.id) {
+                $scope.winOrLose = "Congratulation! You are a word wizard! You won!!!";
+            } else {
+                for (var player in $scope.otherPlayers) {
+                    if (+$scope.otherPlayers[player].id === +winnersArray[0]) {
+                        var winner = $scope.otherPlayers[player].username;
+                        $scope.winOrLose = "Tough luck. " + winner + " has beaten you. Better Luck next time. :("
                     }
                 }
             }
-        }
-        else{
-            let winners=[];
-            for (var i in winnersArray){
-                if (+winnersArray[i]===+$scope.user.id){winners.push($scope.user.username);}
-                else{
-                    for (var player in $scope.otherPlayers){
-                        if ($scope.otherPlayers[player].id==winnersArray[i]){
+        } else {
+            let winners = [];
+            for (var i in winnersArray) {
+                if (+winnersArray[i] === +$scope.user.id) { winners.push($scope.user.username); } else {
+                    for (var player in $scope.otherPlayers) {
+                        if ($scope.otherPlayers[player].id == winnersArray[i]) {
                             winners.push($scope.otherPlayers[player].username);
                             break;
                         }
                     }
                 }
-            console.log(winners);
-            $scope.winOrLose="The game was a tie between ";
-            for (var i=0; i<winners.length; i++){
-                if (i===winners.length-1){$scope.winOrLose+="and "+winners[i]+".";}
-                else{$scope.winOrLose+=winners[i]+", ";}
+                console.log(winners);
+                $scope.winOrLose = "The game was a tie between ";
+                for (var i = 0; i < winners.length; i++) {
+                    if (i === winners.length - 1) { $scope.winOrLose += "and " + winners[i] + "."; } else { $scope.winOrLose += winners[i] + ", "; }
+                }
             }
         }
     }
-}
 
     $rootScope.hideNavbar = true;
 
@@ -332,7 +423,7 @@ app.controller('GameCtrl', function($scope, BoardFactory, Socket, $stateParams, 
 
         Socket.on('gameOver', function(winnersArray) {
             $scope.clear();
-            $scope.freeze=true;
+            $scope.freeze = true;
             $scope.determineWinner(winnersArray);
             $scope.$evalAsync();
             console.log('game is over, winners: ', winnersArray);
