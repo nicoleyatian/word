b2e1eb5f81758c3396c1eb4b1622c0db0aa31e2f
Nathan Wallace <ncw232@gmail.com>
2016-10-11 17:11:19 -0400

    working on it...

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -9,6 +9,7 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
             scope.items = [
                 { label: 'Home', state: 'home' },
                 { label: 'Leader Board', state: 'leaderBoard' },
+                { label: 'How to Play', state: 'Instructions'},
                 { label: 'Your Profile', state: 'UserProfile', auth: true }
             ];
 
