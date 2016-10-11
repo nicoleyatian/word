59f8a67baeab391ab7537e80c3a35f65878395c1
Nathan Wallace <ncw232@gmail.com>
2016-10-11 19:24:04 -0400

    how to play

--- browser/js/instructions/instructions.state.js
+++ browser/js/instructions/instructions.state.js
@@ -1,6 +1,9 @@
 app.config(function($stateProvider){
 	$stateProvider.state("Instructions", {
 		url: "/instructions",
-		templateUrl: "js/instructions/instructions.html"
+		templateUrl: "js/instructions/instructions.html",
+		controller: function($scope){
+			$scope.clicked=false;
+		}
 	})
 })
